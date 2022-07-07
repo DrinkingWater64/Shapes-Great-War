4d6d5d683eb911bf289fe94afde2986b4d927326
drinkingWater <aamuti64@gmail.com>
2022-07-07 20:04:55 +0600

    basic nodes

--- Assets/BT/BehaviourTree.cs
+++ Assets/BT/BehaviourTree.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviourTree : MonoBehaviour
+[CreateAssetMenu()]
+public class BehaviourTree : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Node rootNode;
+    public Node.State treeSate = Node.State.Running;
 
-    // Update is called once per frame
-    void Update()
+    public Node.State Update()
     {
-        
+        return rootNode.Update();
     }
+        
 }

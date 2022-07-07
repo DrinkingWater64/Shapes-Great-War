725e9b3342538eaf0f8ca86df0b9d37318301937
drinkingWater <aamuti64@gmail.com>
2022-07-07 21:12:10 +0600

    waitnode created

--- Assets/BT/BehaviourTreeRunner.cs
+++ Assets/BT/BehaviourTreeRunner.cs
@@ -9,12 +9,39 @@ public class BehaviourTreeRunner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tree = ScriptableObject.CreateInstance<BehaviourTree>();
-        var log = ScriptableObject.CreateInstance<DebugLogNode>();
-        log.message = "Running BT";
 
+
+
+        var wait0 = ScriptableObject.CreateInstance<WaitNode>();
+
+        tree = ScriptableObject.CreateInstance<BehaviourTree>();
+        var log0 = ScriptableObject.CreateInstance<DebugLogNode>();
+        log0.message = "Running BT  00000000000";
+        var log1 = ScriptableObject.CreateInstance<DebugLogNode>();
+        var wait1 = ScriptableObject.CreateInstance<WaitNode>();
+        log1.message = "Running BT 1111111111111";
+        var log2 = ScriptableObject.CreateInstance<DebugLogNode>();
+        var wait2 = ScriptableObject.CreateInstance<WaitNode>();
+        log2.message = "Running BT 222222222222222";
+
+
+
+
+
+        var sequencerTest = ScriptableObject.CreateInstance<SequencerNode>();
+        sequencerTest.children.Add(log0);
+        sequencerTest.children.Add(wait0);
+        sequencerTest.children.Add(log1);
+        sequencerTest.children.Add(wait1);
+        sequencerTest.children.Add(log2);
+        sequencerTest.children.Add(wait2);
+
+        
         var loopNode = ScriptableObject.CreateInstance<RepeatNode>();
-        loopNode.child = log;
+        loopNode.child = sequencerTest;
+
+
+
 
         tree.rootNode = loopNode;
     }
--- Assets/BT/SequencerNode.cs
+++ Assets/BT/SequencerNode.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SequencerNode : MonoBehaviour
+public class SequencerNode : CompositeNode
 {
-    // Start is called before the first frame update
-    void Start()
+    int current;
+    protected override void OnStart()
     {
-        
+        current = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnStop()
     {
-        
+
+    }
+
+    protected override State OnUpdate()
+    {
+        var child = children[current];
+        switch (child.Update())
+        {
+            case State.Running:
+                return State.Running;
+            case State.Failure:
+                return State.Failure;
+            case State.Success:
+                current++;
+                break;
+        }
+
+        if (current == children.Count)
+        {
+            return State.Success;
+        }
+        else
+        {
+            return State.Running;
+        }
+
     }
 }
